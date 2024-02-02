6bc585b567ad6e411750c86eff343ab7fdcadfaf
Jakub Polacek <Kuba.krtek@email.cz>
2024-02-02 16:38:51 +0100

    Finished question 5

--- src/main/java/Question5.java
+++ src/main/java/Question5.java
@@ -2,30 +2,50 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-
 /**
  * Jakub Polacek
  * GD2b
  * 2.2.2024
  */
+
 public class Question5
 {
     public static void main(String[] args)
     {
         Queue<String> takeOffQueue = new LinkedList<>();
         Queue<String> landingQueue = new LinkedList<>();
-        Scanner input = new Scanner(System.in);
+        Scanner input;
         String command = "";
+
+        System.out.println("*********************************************");
+        System.out.println("Welcome to the simulation of Airport Control!");
+        System.out.println("*********************************************");
+        System.out.println("The list of available commands:");
+        System.out.println("- \"takeoff <flightSymbol>\" -> places the flight into queue for take-off (ex. 'takeoff FR526')");
+        System.out.println("- \"land <flightSymbol>\" -> places the flight into queue for landing (ex. 'land KC95DF')");
+        System.out.println("- \"next\" -> finishes current take-off or landing and prints its symbol");
+        System.out.println("- \"quit\" -> ends the simulation");
 
         while(!command.equals("quit"))
         {
-            System.out.println("Enter your command:");
+            System.out.println("\nEnter your command:");
             input = new Scanner(System.in);
             command = input.nextLine();
 
             if(command.equals("next"))
             {
-                //TODO loads
+                if(!landingQueue.isEmpty())
+                {
+                    System.out.println("The flight of number " + landingQueue.remove() + " landed into the airport.");
+                }
+                else if(!takeOffQueue.isEmpty())
+                {
+                    System.out.println("The flight of number " + takeOffQueue.remove() + " took off from the airport.");
+                }
+                else
+                {
+                    System.out.println("No flight waiting landing or take-off queues.");
+                }
             }
             else if(command.matches("land [A-Za-z0-9]+"))
             {
@@ -35,10 +55,10 @@ public class Question5
             {
                 takeOffQueue.add(command.substring(8));
             }
-
-            System.out.println(landingQueue.peek());
-            System.out.println(takeOffQueue.peek());
-
+            else
+            {
+                System.out.println("Wrong input.");
+            }
         }
         System.out.println("Goodbye");
     }
