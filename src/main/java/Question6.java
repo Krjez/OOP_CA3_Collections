d06ff10a0e4e5382230dc443375db3d11f71258e
Jakub Polacek <Kuba.krtek@email.cz>
2024-02-08 16:35:17 +0100

    Rewritten code input into switch

--- src/main/java/Question6.java
+++ src/main/java/Question6.java
@@ -16,25 +16,20 @@ public class Question6
             System.out.println("Enter the command:");
             input = new Scanner(System.in);
             command = input.next();
-            
-            if(command.matches("buy [0-9]+ [0-9]*\\.[0-9]+"))
-            {
-                String[] commandArr = command.split("\\s+");
-                stocks.add(new Block(Integer.parseInt(commandArr[1]),Double.parseDouble(commandArr[2])));
 
-            }
-            else if(command.matches("sell [0-9]+ [0-9]*\\.[0-9]+"))
-            {
-                String[] commandArr = command.split("\\s+");
+            switch (command) {
+                case "buy" -> {
+                    int quantity = input.nextInt();
+                    double price = input.nextDouble();
+                    stocks.add(new Block(quantity, price));
+                }
+                case "sell" -> {
+                    int quantity = input.nextInt();
+                    double price = input.nextDouble();
 
-            }
-            else if(command.equals("quit"))
-            {
-                System.out.println("Goodbye");
-            }
-            else
-            {
-                System.out.println("Invalid input, try again.");
+                }
+                case "quit" -> System.out.println("Goodbye");
+                default -> System.out.println("Invalid input, try again.");
             }
 
         }
